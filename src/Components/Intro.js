70491d1cfbace10060aa6855b7ad1e169fec364a
rb <rb@>
2021-03-24 17:32:43 +0300

    updt

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -5,13 +5,15 @@ class Intro extends React.Component {
   render () {
     return(
       <div className="intro">
-        <div className="bg"style={{background: 'url(./src/intro.jpg)'}}/>
+        <div className="bg"style={{backgroundImage: 'url(./src/intro.jpg)'}}/>
         <div className="menu">
           <div className="logo"><span>S</span>uperdim</div>
           <div className="tel"><a href="tel:+380952264040">+38 095 226 40 40</a></div>
         </div>
-        <h1><span>БУРЕНИЕ СКВАЖИН</span> В ЧЕРКАССАХ</h1>
-        <h2><span>СКИДКИ</span> на бурение скважин на воду в Черкассах</h2>
+        <div className="text">
+          <h1><span>БУРЕНИЕ СКВАЖИН</span> <br/>В ЧЕРКАССАХ</h1>
+          <h2><span>СКИДКИ</span> на бурение скважин на воду в Черкассах</h2>
+        </div>
         <div className="btns">
           <div className="btn btn_call">Позвонить</div>
           <div className="btn btn_re_call">Заказать звонок</div>
