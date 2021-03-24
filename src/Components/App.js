332d4f1c1008f0f3c53d7c64fcf7c86e4ea2d452
rb <rb@>
2021-03-24 15:51:08 +0300

    init

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import Intro from './Intro';
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class App extends React.Component {
+  render () {
+    return(
+      <div className="App">
+      <Intro/>
+    </div>
+  )
+  }
+}
+
+export default App;
---
+++ src/Components/Intro.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Intro extends React.Component {
+  render () {
+    return(
+      <div className="intro">
+        <div className="bg"style={{background: 'url(./src/intro.jpg)'}}/>
+        <div className="menu">
+          <div className="logo"><span>S</span>uperdim</div>
+          <div className="tel"><a href="tel:+380952264040">+38 095 226 40 40</a></div>
+        </div>
+        <h1><span>БУРЕНИЕ СКВАЖИН</span> В ЧЕРКАССАХ</h1>
+        <h2><span>СКИДКИ</span> на бурение скважин на воду в Черкассах</h2>
+        <div className="btns">
+          <div className="btn btn_call">Позвонить</div>
+          <div className="btn btn_re_call">Заказать звонок</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Intro;
